5d7ae4a75b7ef888f136cc89dbb0bad706eae31f
Neeraj Sharma <nierraj@gmail.com>
2021-05-25 18:45:50 +0530

    Adding Swagger 3

--- src/main/java/com/product/catalogue/search/ProductCatalogueSearchApplication.java
+++ src/main/java/com/product/catalogue/search/ProductCatalogueSearchApplication.java
@@ -2,13 +2,30 @@ package com.product.catalogue.search;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableJpaRepositories
 @SpringBootApplication
+@EnableSwagger2
 public class ProductCatalogueSearchApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductCatalogueSearchApplication.class, args);
 	}
+	
+	@Bean
+    public Docket api() { 
+        return new Docket(DocumentationType.SWAGGER_2)  
+          .select()                                  
+          .apis(RequestHandlerSelectors.any())              
+          .paths(PathSelectors.any())                          
+          .build();                                           
+    }
 }
